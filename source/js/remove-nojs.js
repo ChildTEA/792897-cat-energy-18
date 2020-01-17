1e4ba2b2845f4922dfccfd093960264a07b41dcf
Yaroslav Smirnov <yar14@mail.ru>
2020-01-17 18:38:36 +0300

    Добавлен fallback для карты при отключенном JS

---
+++ source/js/remove-nojs.js
@@ -0,0 +1,5 @@
+var mainNav = document.querySelector('.main-nav');
+var mapFallback = document.querySelector('.contacts__map-fallback');
+
+mainNav.classList.remove('main-nav--nojs');
+mapFallback.classList.remove('contacts__map-fallback--nojs');
